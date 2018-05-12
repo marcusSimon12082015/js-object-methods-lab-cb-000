faa2126ad80be885adcaa1f413723b80d44df4c1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-12 04:50:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ class President
     this.yearsInOffice = yearsInOffice;
     this.homeState = homeState;
   }
-  
+  veto(){ return("No");}
+  passBill(){ return("You can do that");}
+  doCharity(){ return("I like to help people");}
+  conductPressInterview(){ return("I am proud to be an American");}
+  sayHi(){ return("Hi, my name is "+ this.name +". I am from "+this.homeState+". I present the "+
+                  this.politicalParty+"s, and was in office "+this.yearsInOffice+".");}
 }
