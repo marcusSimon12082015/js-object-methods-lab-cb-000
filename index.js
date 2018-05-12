ed1e7319d9f02d9140c1135d499c6b6d0005f341
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-12 04:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code solution here
+class President
+{
+  constructor(name,politicalParty,yearsInOffice,homeState)
+  {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+  
+}
