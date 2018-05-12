1da64a21ae9dfd17b74f0a38f2b472f51ce7d792
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-12 04:51:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class President
     this.homeState = homeState;
   }
   veto(){ return("No!");}
-  passBill(){ return("You can do that");}
+  passBill(){ return("You can do that!");}
   doCharity(){ return("I like to help people");}
   conductPressInterview(){ return("I am proud to be an American");}
   sayHi(){ return("Hi, my name is "+ this.name +". I am from "+this.homeState+". I present the "+
