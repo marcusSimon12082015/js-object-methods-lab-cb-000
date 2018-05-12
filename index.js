4a9ac7e4fae67c85dce1cae9e9e03c5054af4864
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-12 04:55:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class President
   passBill(){ return("You can do that!");}
   doCharity(){ return("I like to help people.");}
   conductPressInterview(){ return("I am proud to be an American.");}
-  sayHi(){ return("Hi, my name is "+ this.name +". I am from "+this.homeState+". I present the "+
+  sayHi(){ return("Hi, my name is "+ this.name +". I am from "+this.homeState+". I represent the "+
                   this.politicalParty+"s, and was in office "+this.yearsInOffice+".");}
 }
 
